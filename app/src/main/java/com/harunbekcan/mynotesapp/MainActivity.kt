627b82109962420d3af1d33a0632c33fa685db06
Harun Bekcan <harunbekcan@gmail.com>
2022-10-31 20:09:03 +0300

    mainActivity

--- app/src/main/java/com/harunbekcan/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/harunbekcan/mynotesapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.harunbekcan.mynotesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.harunbekcan.mynotesapp.base.BaseActivity
+import com.harunbekcan.mynotesapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun getLayoutId(): Int = R.layout.activity_main
+
+    override fun prepareView(savedInstanceState: Bundle?) {}
+
 }
