7621ac1e2e74c955f15df2c581e184bbf3b30b67
Harun Bekcan <harunbekcan@gmail.com>
2022-11-13 13:47:20 +0300

    myNotesViewModel added

---
+++ app/src/main/java/com/harunbekcan/mynotesapp/ui/fragment/MyNotesViewModel.kt
@@ -0,0 +1,8 @@
+package com.harunbekcan.mynotesapp.ui.fragment
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyNotesViewModel @Inject constructor() {
+}
