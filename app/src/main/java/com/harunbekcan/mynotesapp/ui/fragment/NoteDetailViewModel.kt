32e7ce9dff1f0a826e254fbc314fb71dde1195e3
Harun Bekcan <harunbekcan@gmail.com>
2022-11-14 20:43:00 +0300

    noteDetailViewModel created

---
+++ app/src/main/java/com/harunbekcan/mynotesapp/ui/fragment/NoteDetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.harunbekcan.mynotesapp.ui.fragment
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteDetailViewModel @Inject constructor() : ViewModel() {
+}
