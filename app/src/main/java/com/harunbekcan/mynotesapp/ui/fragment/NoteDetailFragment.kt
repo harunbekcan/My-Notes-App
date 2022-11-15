a84d579ba7c7755221fe53a9b2aaa1d17ca67c44
Harun Bekcan <harunbekcan@gmail.com>
2022-11-15 11:22:03 +0300

    noteDetailPage toolbar added

--- app/src/main/java/com/harunbekcan/mynotesapp/ui/fragment/NoteDetailFragment.kt
+++ app/src/main/java/com/harunbekcan/mynotesapp/ui/fragment/NoteDetailFragment.kt
@@ -1,14 +1,31 @@
 package com.harunbekcan.mynotesapp.ui.fragment
 
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 import com.harunbekcan.mynotesapp.R
 import com.harunbekcan.mynotesapp.base.BaseFragment
 import com.harunbekcan.mynotesapp.databinding.FragmentNoteDetailBinding
+import com.harunbekcan.mynotesapp.utils.setVisible
 
 class NoteDetailFragment : BaseFragment<FragmentNoteDetailBinding>() {
 
     override fun getLayoutId(): Int = R.layout.fragment_note_detail
 
-    override fun prepareView(savedInstanceState: Bundle?) {}
+    override fun prepareView(savedInstanceState: Bundle?) {
+        initToolbar()
+    }
 
+    private fun initToolbar(){
+        binding.myNoteDetailToolbar.apply {
+            backButtonImageView.setVisible()
+            toolbarBackButtonListener(backButtonImageView)
+        }
+    }
+
+    private fun toolbarBackButtonListener(backButton:ImageView) {
+        backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
