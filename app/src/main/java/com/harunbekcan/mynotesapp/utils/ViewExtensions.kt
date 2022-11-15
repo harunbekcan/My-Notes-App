8c21e8aab2ec711f74a39460a710feee97f466b9
Harun Bekcan <harunbekcan@gmail.com>
2022-11-15 11:14:28 +0300

    toolbarView backButton added

---
+++ app/src/main/java/com/harunbekcan/mynotesapp/utils/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.harunbekcan.mynotesapp.utils
+
+import android.view.View
+
+fun View.setGone(){
+    this.visibility  = View.GONE
+}
+
+fun View.setVisible(){
+    this.visibility  = View.VISIBLE
+}
