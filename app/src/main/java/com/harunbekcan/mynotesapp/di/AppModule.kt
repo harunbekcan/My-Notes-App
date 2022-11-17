fbc6e6a95e85110a54cfc6a1756ab945bbdd53c4
Harun Bekcan <harunbekcan@gmail.com>
2022-11-17 15:41:57 +0300

    injectRoomDb added

--- app/src/main/java/com/harunbekcan/mynotesapp/di/AppModule.kt
+++ app/src/main/java/com/harunbekcan/mynotesapp/di/AppModule.kt
@@ -1,4 +1,15 @@
 package com.harunbekcan.mynotesapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.harunbekcan.mynotesapp.roomdb.NoteDatabase
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun injectRoomDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(context, NoteDatabase::class.java,"NoteDB").build()
 }
