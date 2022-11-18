62ed2eba14ed0a6bdbce05e29e5ea998f09d8e8f
Harun Bekcan <harunbekcan@gmail.com>
2022-11-18 17:14:14 +0300

    noteRepository injected

--- app/src/main/java/com/harunbekcan/mynotesapp/data/repo/NoteRepositoryInterface.kt
+++ app/src/main/java/com/harunbekcan/mynotesapp/data/repo/NoteRepositoryInterface.kt
@@ -1,4 +1,15 @@
 package com.harunbekcan.mynotesapp.data.repo
 
+import androidx.lifecycle.LiveData
+import com.harunbekcan.mynotesapp.data.entity.Note
+
 interface NoteRepositoryInterface {
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+
+    suspend fun updateNote(note: Note)
+
+    fun getNote() : LiveData<List<Note>>
 }
--- app/src/main/java/com/harunbekcan/mynotesapp/di/AppModule.kt
+++ app/src/main/java/com/harunbekcan/mynotesapp/di/AppModule.kt
@@ -2,6 +2,9 @@ package com.harunbekcan.mynotesapp.di
 
 import android.content.Context
 import androidx.room.Room
+import com.harunbekcan.mynotesapp.data.repo.NoteRepository
+import com.harunbekcan.mynotesapp.data.repo.NoteRepositoryInterface
+import com.harunbekcan.mynotesapp.roomdb.NoteDao
 import com.harunbekcan.mynotesapp.roomdb.NoteDatabase
 import dagger.Provides
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -12,5 +15,13 @@ object AppModule {
     @Singleton
     @Provides
     fun injectRoomDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(context, NoteDatabase::class.java,"NoteDB").build()
+
+    @Singleton
+    @Provides
+    fun injectDao(database: NoteDatabase) = database.noteDao()
+
+    @Singleton
+    @Provides
+    fun injectNoteRepository(dao : NoteDao) = NoteRepository(dao) as NoteRepositoryInterface
 
 }
