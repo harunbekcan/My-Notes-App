8aca9f1da294ff5eb53409340da4c71586ee8186
Harun Bekcan <harunbekcan@gmail.com>
2022-11-30 12:31:19 +0300

    noteDetailPageUi completed

--- app/src/main/java/com/harunbekcan/mynotesapp/data/entity/Note.kt
+++ app/src/main/java/com/harunbekcan/mynotesapp/data/entity/Note.kt
@@ -1,12 +1,14 @@
 package com.harunbekcan.mynotesapp.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "notes")
-data class Note (
-    var title : String,
-    var description : String,
-    @PrimaryKey (autoGenerate = true)
-    var id : Int ?= null
-)
+data class Note(
+    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "description") var description: String,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") var id: Int? = null
+) : Serializable
--- app/src/main/java/com/harunbekcan/mynotesapp/ui/fragment/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/harunbekcan/mynotesapp/ui/fragment/notedetail/NoteDetailFragment.kt
@@ -3,6 +3,7 @@ package com.harunbekcan.mynotesapp.ui.fragment.notedetail
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.harunbekcan.mynotesapp.R
 import com.harunbekcan.mynotesapp.base.BaseFragment
 import com.harunbekcan.mynotesapp.databinding.FragmentNoteDetailBinding
@@ -10,16 +11,26 @@ import com.harunbekcan.mynotesapp.utils.setVisible
 
 class NoteDetailFragment : BaseFragment<FragmentNoteDetailBinding>() {
 
+    private val navArgs : NoteDetailFragmentArgs by navArgs()
+
     override fun getLayoutId(): Int = R.layout.fragment_note_detail
 
     override fun prepareView(savedInstanceState: Bundle?) {
         initToolbar()
+        initUi()
     }
 
     private fun initToolbar(){
-        binding.myNoteDetailToolbar.apply {
+        binding.noteDetailToolbar.apply {
             backButtonImageView.setVisible()
             toolbarBackButtonListener(backButtonImageView)
+        }
+    }
+
+    private fun initUi(){
+        navArgs.noteItem.let {
+            binding.noteTitleEditText.setText(it.title)
+            binding.noteDescriptionEditText.setText(it.description)
         }
     }
 
